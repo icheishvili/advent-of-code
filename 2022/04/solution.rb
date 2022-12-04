9128d498cc56c361a2442abf6ad3396173181b88
Ilia Cheishvili <ilia.cheishvili@gmail.com>
2022-12-03 23:53:57 -0700

    2022/04

---
+++ 2022/04/solution.rb
@@ -0,0 +1,25 @@
+require 'active_support/all'
+
+ranges = File.read('input.txt').lines.map do |line|
+  range1, range2 = line.chomp.split(',')
+
+  range1 = range1.split('-').map(&:to_i)
+  range2 = range2.split('-').map(&:to_i)
+  
+  range1 = (range1[0]..range1[1])
+  range2 = (range2[0]..range2[1])
+
+  [range1, range2]
+end
+
+coverings = ranges.select do |(r1, r2)|
+  r1.cover?(r2) || r2.cover?(r1)
+end
+
+puts "Part 1: #{coverings.count}"
+
+overlaps = ranges.select do |(r1, r2)|
+  r1.overlaps?(r2) || r2.overlaps?(r1)
+end
+
+puts "Part 1: #{overlaps.count}"
